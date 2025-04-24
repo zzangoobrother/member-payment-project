0eb5c6962cca40d951fbf6289014f8aa69597d98
choiseonkang <com1368@naver.com>
2025-04-25 06:53:29 +0900

    feat : init

---
+++ src/test/java/com/example/memberpaymentproject/MemberPaymentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memberpaymentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberPaymentProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
