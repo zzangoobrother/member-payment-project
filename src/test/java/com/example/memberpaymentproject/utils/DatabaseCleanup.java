b10848c990275dae2e3f55a14a18b074a2f3d127
choiseonkang <com1368@naver.com>
2025-04-27 14:30:53 +0900

    feat : 프로필 조회수 업데이트 인수테스트 작

---
+++ src/test/java/com/example/memberpaymentproject/utils/DatabaseCleanup.java
@@ -0,0 +1,34 @@
+package com.example.memberpaymentproject.utils;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Profile("test")
+@Component
+public class DatabaseCleanup implements InitializingBean {
+
+    private List<String> tableNames;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        tableNames = jdbcTemplate.query("SHOW TABLES", (resultSet, rowNumber) -> resultSet.getString(1));
+    }
+
+    @Transactional
+    public void execute() {
+        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY FALSE");
+        for (String tableName : tableNames) {
+            jdbcTemplate.execute("TRUNCATE TABLE " + tableName);
+        }
+        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY TRUE");
+    }
+}
