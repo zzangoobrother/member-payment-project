d0c606a92a8f27dcdde595a6aeb39df3de63cd5d
choiseonkang <com1368@naver.com>
2025-04-27 21:04:06 +0900

    feat : toss payment 결제 실패시 재처리 구현

--- src/main/java/com/example/memberpaymentproject/interfaces/feign/PaymentClient.java
+++ src/main/java/com/example/memberpaymentproject/interfaces/feign/PaymentClient.java
@@ -1,12 +1,13 @@
 package com.example.memberpaymentproject.interfaces.feign;
 
+import com.example.memberpaymentproject.global.exception.feign.PaymentFeignClientErrorDecoder;
 import com.example.memberpaymentproject.interfaces.feign.request.TossPaymentRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(value = "paymentClient", url = "https://api.tosspayments.com/v1")
+@FeignClient(value = "paymentClient", url = "https://api.tosspayments.com/v1", configuration = PaymentFeignClientErrorDecoder.class)
 public interface PaymentClient {
 
     @PostMapping(value = "/payments/confirm", consumes = "application/json; charset=UTF-8")
